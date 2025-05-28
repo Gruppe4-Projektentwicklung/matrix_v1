7dbda9b58060e4295beaff8ac6c175379b9a090a
Gruppe4-Projektentwicklung <gregor.kordowich.student@gmail.com>
2025-05-29 00:31:04 +0200

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+[Dateien]
+currentidealist = ideen1.xlsx //datei aus matrix_v1/ideacollection/"Dateiname"
+currentcombinationlist = kombis1.xlsx  //datei aus matrix_v1/ideacollection/"Dateiname"
+ideentemplate = templates/ideen-vorlage.xlsx
+kombitemplate = templates/kombi-vorlage.xlsx
+
+[Features]
+dev_mode_button = true
+datapopup = on
+testerbutton = off
