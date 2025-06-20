4457603a6858069be8d6096de5021304612073da
Gruppe4-Projektentwicklung <gregor.kordowich.student@gmail.com>
2025-06-20 13:09:23 +0200

    Add footer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,6 +32,7 @@ import type { SelectChangeEvent } from "@mui/material/Select";
 import { getSessionId, setPageStatus } from "./utils/session";
 import { devConfig } from "./devConfig";
 import { DevStatusBar } from "./components/DevStatusBar";
+import { Footer } from "./components/Footer";
 import { calculateRanking } from "./api/calculateRanking";
 
 function App() {
@@ -487,6 +488,7 @@ function App() {
           type={statusToastType}
         />
       </Container>
+      <Footer />
     </div>
   );
 
