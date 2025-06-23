159a97fe7da9fb871862225ed9d9f04d9cb7ff77
Gruppe4-Projektentwicklung <gregor.kordowich.student@gmail.com>
2025-06-23 22:57:45 +0200

    Fix StartPage layout

--- frontend/src/pages/StartPage.tsx
+++ frontend/src/pages/StartPage.tsx
@@ -19,13 +19,23 @@ export const StartPage = ({ dev2Mode, onStart }: Props) => {
     navigate('/select-data');
   };
   return (
-  <PageContainer className="min-h-[60vh] my-6 text-center">
-    <Typography variant="h4" component="h1" mb={4} color="primary">
+  <PageContainer className="min-h-[60vh] my-6">
+    <Typography
+      variant="h4"
+      component="h1"
+      mb={4}
+      color="primary"
+      align="center"
+    >
       {t('title')}
     </Typography>
-    <Typography mb={3} color="text.secondary">
-      {t('introText')}
-    </Typography>
+    <Typography
+      component="div"
+      mb={3}
+      color="text.secondary"
+      sx={{ textAlign: 'left' }}
+      dangerouslySetInnerHTML={{ __html: t('introText') }}
+    />
     <Box mt={4} display="flex" justifyContent="center">
       <Button variant="contained" color="primary" onClick={handleStart}>
         {t('start')}
